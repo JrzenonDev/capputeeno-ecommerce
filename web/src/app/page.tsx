9b4d1b95ee307979605c79dc53525ac422a42be0
JrzenonDev <jrobertoonb@gmail.com>
2023-09-01 01:48:59 -0300

    feat: create component DefaultPageLayout

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -3,12 +3,15 @@
 import { FilterBar } from "@/components/filterBar/FilterBar";
 import { ProducList } from "@/components/productList/ProductList";
 import { PageWrapper } from "./style";
+import { DefaultPageLayout } from "@/components/defaultPageLayout/DefaultPageLayout";
 
 export default function Home() {
   return (
-    <PageWrapper>
-      <FilterBar />
-      <ProducList />
-    </PageWrapper>
+    <DefaultPageLayout>
+      <PageWrapper>
+        <FilterBar />
+        <ProducList />
+      </PageWrapper>
+    </DefaultPageLayout>
   );
 }
--- web/src/app/style.ts
+++ web/src/app/style.ts
@@ -4,11 +4,4 @@ export const PageWrapper = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 12px 24px;
-  height: 100vh;
-  background-color: var(--bg-primary);
-
-  @media (min-width: ${(props) => props.theme.desktopBreakpoint}) {
-    padding: 34px 160px;
-  }
 `;
