3e350ff0416bf4542786f89f0dc2548cb697554a
JrzenonDev <jrobertoonb@gmail.com>
2023-08-31 22:58:45 -0300

    fix: fixing validation when localStorage is empty

--- web/src/components/cartControl/CartControl.jsx
+++ web/src/components/cartControl/CartControl.jsx
@@ -3,11 +3,12 @@ import { useLocalStorage } from "../../hooks/useLocalStorage";
 import { Container, CartCount } from "./style";
 
 export function CartControl() {
-  const { value } = useLocalStorage("cart-items");
+  const { value } = useLocalStorage("cart-items", []);
+
   return (
     <Container>
       <CartIcon />
-      {value.length > 0 && <CartCount>{value.length}</CartCount>}
+      {value.length && <CartCount>{value.length}</CartCount>}
     </Container>
   );
 }
--- web/src/hooks/useLocalStorage.ts
+++ web/src/hooks/useLocalStorage.ts
@@ -1,19 +1,17 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage<T>(item: string, initialValue: T) {
-  const [value, setValue] = useState<T>(() => {
-    if (typeof window === "undefined") return initialValue;
-    let storedValue = localStorage.getItem(item);
-    return storedValue ? JSON.parse(storedValue) : initialValue;
-  });
+  const [value, setValue] = useState<T>(initialValue);
 
   useEffect(() => {
     if (typeof window === "undefined") return;
-    localStorage.setItem(item, JSON.stringify(value));
-  }, [item, value]);
+    let value = localStorage.getItem(item);
+    if (value) setValue(JSON.parse(value));
+  }, [window]);
 
   const updateLocalStorage = (newValue: T) => {
     setValue(newValue);
+    localStorage.setItem(item, JSON.stringify(newValue));
   };
 
   return {
