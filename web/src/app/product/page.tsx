41b4cb60332aaa5f0b4a65aeda69b12375ebd45a
JrzenonDev <jrobertoonb@gmail.com>
2023-09-03 11:19:16 -0300

    feat: getting data useProduct and styles in page product

--- web/src/app/product/page.tsx
+++ web/src/app/product/page.tsx
@@ -1,17 +1,45 @@
 "use client";
 
 import { DefaultPageLayout } from "@/components/defaultPageLayout/DefaultPageLayout";
-import { Container } from "./style";
+import { Container, ProductInfo } from "./style";
 import { BackButton } from "@/components/backButton/BackButton";
+import { useProduct } from "@/hooks/useProduct";
+import { FormatValue } from "@/utils/formatPrice";
+import { ShoppingBagIcon } from "@/components/icons/ShoppingBagIcon";
 
 interface ProductsProps {}
 
-export default function Products(props: ProductsProps) {
+export default function Products({
+  searchParams,
+}: {
+  searchParams: { id: string };
+}) {
+  const { data } = useProduct(searchParams.id);
   return (
     <DefaultPageLayout>
       <Container>
         <BackButton navigate="/" />
-        <section>Informações do produto</section>
+        <section>
+          <img src={data?.image_url} />
+          <div>
+            <ProductInfo>
+              <span>{data?.category}</span>
+              <h2>{data?.name}</h2>
+              <span>{FormatValue(data?.price_in_cents ?? 0)}</span>
+              <p>
+                *Frete de R$40,00 para todo o Brasil. Grátis para compras acima
+                de R$900,00.
+              </p>
+              <div>
+                <h3>Descrição</h3>
+                <p>{data?.description}</p>
+              </div>
+            </ProductInfo>
+            <button>
+              <ShoppingBagIcon /> Adicionar ao carrinho
+            </button>
+          </div>
+        </section>
       </Container>
     </DefaultPageLayout>
   );
