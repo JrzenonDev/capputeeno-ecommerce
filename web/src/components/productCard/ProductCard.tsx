cf93be14ff7d8903de8474151f8893d528d2ca2a
JrzenonDev <jrobertoonb@gmail.com>
2023-09-03 23:52:34 -0300

    feat: creating navigate product page

--- web/src/components/productCard/ProductCard.tsx
+++ web/src/components/productCard/ProductCard.tsx
@@ -1,16 +1,23 @@
 import { FormatValue } from "@/utils/formatPrice";
 import { Card } from "./style";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
   image: string;
   title: string;
   price: number;
+  id: string;
 }
 
 export function ProductCard(props: ProductCard) {
+  const router = useRouter();
   const price = FormatValue(props.price);
+
+  const handleNavigate = () => {
+    router.push("/product?id=" + props.id);
+  };
   return (
-    <Card>
+    <Card onClick={handleNavigate}>
       <img src={props.image} />
       <div>
         <h3>{props.title}</h3>
